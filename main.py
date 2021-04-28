cd74b6aebb3f787f4279143929992bd0d2a62564
takeru1205 <takeru0639418@gmail.com>
2021-04-28 16:30:54 +0900

    [update] store transition to buffer

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import gym
+
+from buffer import ReplayBuffer
 gym.logger.set_level(40)
 
 num_episode = 5
 
 env = gym.make('Pendulum-v0')
+buffer = ReplayBuffer(max_size=100)
 
 for e in range(num_episode):
     cumulative_reward = 0
@@ -12,8 +15,13 @@ for e in range(num_episode):
         action = env.action_space.sample()
 
         next_state, reward, done, info = env.step(action)
+        buffer.add([state, next_state, reward, done])
+
+        state = next_state
 
         cumulative_reward += reward
         
     print(f'Episode: {e:>3}, Reward: {cumulative_reward:>8.2f}')
+
+print(len(buffer))
 
