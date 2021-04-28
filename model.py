dcce7ea4259b22799ad7b34a060bfa226da8839b
takeru1205 <takeru0639418@gmail.com>
2021-04-28 17:46:11 +0900

    [add] reparameterization trick

--- model.py
+++ model.py
@@ -1,6 +1,25 @@
+import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
+
+def calculate_log_pi(log_stds, noises, actions):
+    gaussian_log_probs = (-0.5 * noises.pow(2) - log_stds).sum(dim=-1, keepdim=True) - 0.5 * math.log(2 * math.pi) * log_stds.size(-1)
+
+    log_pis = gaussian_log_probs - torch.log(1 - actions.pow(2) + 1e-6).sum(dim=-1, keepdim=True)
+    return log_pis
+
+
+def reparameterize(means, log_stds):
+    stds = log_stds.exp()
+    noises = torch.randn_like(means)
+    us = means + noises * stds
+    actions = torch.tanh(us)
+
+    log_pis = calculate_log_pi(log_stds), noises, actions
+
+    return actions, log_pis
 
 
 class Actor(nn.Module):
@@ -14,6 +33,14 @@ class Actor(nn.Module):
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = torch.tanh(self.fc3(x))
+        x = self.fc3(x)
+        x = x.chunk(2, dim=-1)[0]
         return x
+
+    def sample(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        means, log_stds = x.chunk(2, dim=-1)
+        return reparameterize(means, log_stds.clamp_(-20, 2))
 
