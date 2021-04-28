c01a8fe22dab10a8f2885e6454f9020aa7900f44
takeru1205 <takeru0639418@gmail.com>
2021-04-28 15:57:03 +0900

    [update] loop some episodes

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import gym
 gym.logger.set_level(40)
 
+num_episode = 5
+
 env = gym.make('Pendulum-v0')
 
+for e in range(num_episode):
+    cumulative_reward = 0
+    state = env.reset()
+    for i in range(env.spec.max_episode_steps):
+        action = env.action_space.sample()
 
-state = env.reset()
-cumulative_reward = 0
-for i in range(env.spec.max_episode_steps):
-    action = env.action_space.sample()
+        next_state, reward, done, info = env.step(action)
 
-    next_state, reward, done, info = env.step(action)
-
-    cumulative_reward += reward
-    
-print(f'Reward: {cumulative_reward:>8.2f}')
+        cumulative_reward += reward
+        
+    print(f'Episode: {e:>3}, Reward: {cumulative_reward:>8.2f}')
 
